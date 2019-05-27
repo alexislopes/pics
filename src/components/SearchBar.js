2cccd425797c32b982aac865abde970ca2c91235
alexislopes <alexisdrops@gmail.com>
2019-05-27 13:00:53 -0300

    preventing the browser to refresh on a form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,17 +2,24 @@ import React from  'react';
 
 class SearchBar extends React.Component {
 
-    onInputChange(event) {
-        console.log(event.target.value)
+    state = {term: ''}
+
+    onFormSubmit(event) {
+        event.preventDefault()
+        console.log(this.state.term)
     }
 
     render() {
         return (
             <div className="ui segment">
-              <form className="ui form">
+              <form onSubmit={this.onFormSubmit} className="ui form">
                   <div className="field">
                       <label>Image Search</label>
-                <input type="text" onChange={this.onInputChange} />
+                      <input 
+                        type="text" 
+                        value={this.state.term}
+                        onChange={(e) => this.setState({term: e.target.value})} 
+                      />
                 </div>
               </form>
             </div>
