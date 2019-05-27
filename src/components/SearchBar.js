3971cec6982d6f88de6541caeb0568081b4b476f
alexislopes <alexisdrops@gmail.com>
2019-05-27 14:00:28 -0300

    dealing with state and props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,9 @@ class SearchBar extends React.Component {
 
     state = {term: ''}
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) =>  {
         event.preventDefault()
-        console.log(this.state.term)
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
